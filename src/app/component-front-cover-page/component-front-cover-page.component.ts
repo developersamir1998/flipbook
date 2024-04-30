8f4068e0c1ef43cdaec372fee6c55185885c0705
livinglabsicets <livinglabsicets@gmail.com>
2024-04-30 15:34:47 +0530

    added logic for fetch data from service file in front cover page

--- src/app/component-front-cover-page/component-front-cover-page.component.ts
+++ src/app/component-front-cover-page/component-front-cover-page.component.ts
@@ -13,12 +13,10 @@ import {MatCardModule} from '@angular/material/card';
   
 })
 export class ComponentFrontCoverPageComponent implements OnInit {
-  feedbackData: any[] = [];
-  currentVisitStatus: string = 'We are checking today visits.';
-  filteredData: any[] = [];
-  selectedClientName: string | null = null;
+  
   recievedClientName!: string | null | '';
-  visitParams: any[]= [];
+  
+  visitorDetails: any[] = [];
 
   constructor(
     private router: Router, 
@@ -26,76 +24,43 @@ export class ComponentFrontCoverPageComponent implements OnInit {
     private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.feedbackDataService.getFilteredData().subscribe(data => {
+      console.log("getfilteredData() method called=",data);
+      
+      if (data ) {
+        // Assuming client_name is a property in the data
+        this.recievedClientName = data.client_name;
+        this.visitorDetails=data.visitors_details;
 
-    this.route.queryParams.subscribe(params=>{
-      const clientName=params['clientName'];
-      const visitDate=params['visitDate'];
-      console.log('Recieved client name:',clientName);
-      console.log('Recieved visit date:',visitDate);
-
-      if(clientName!=undefined)
-        {
-          this.fetchData(clientName);
-          this.isButtonSelected(clientName);
-          this.handleButtonClick(clientName);
-        }
-
+      } else {
+        // Handle the case where filtered data is empty or undefined
+        this.recievedClientName = "Loading Client Details..";
+        console.error('Data recieved from service file is empty');
+      }
+      
     });
 
   }
 
-  // Fetch data from the API
-  fetchData(clientName: string): void {
-    fetch('https://feedback-app-v1-0.onrender.com/api/visit/visitList')
-      .then(response => response.json())
-      .then(data => {
-        this.feedbackData = data;
-        this.filterData();
-        this.handleButtonClick(clientName);
-      })
-      .catch(error => {
-        console.error('Error Fetching Data:', error);
-      });
-  }
-
-  // Utility function to format date as DD-MM-YYYY
-  formatDate(dateString: string): string {
-    const date = new Date(dateString);
-    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
-  }
-
-  // Get current date in DD-MM-YYYY format
-  currentDate: string = this.formatDate(new Date().toISOString().split('T')[0]);
-
-  // Filter data based on the current date
-  filterData(): void {
-    this.filteredData = this.feedbackData.filter(item => this.formatDate(item.visit_date) === this.currentDate);
-    
-    if (this.filteredData.length === 0) {
-      
-      this.currentVisitStatus = 'No Visit Today.';
-    }
-  }
   
-  handleButtonClick(clientName: string): void {
-    const selectedFeedback = this.filteredData.find(item => item.client_name === clientName);
-    console.log("selected Feedback=",selectedFeedback);
-    if (selectedFeedback) {
-      this.selectedClientName = clientName;
-      this.feedbackDataService.setSelectedFeedback(selectedFeedback);
-    }
-  }
 
-  // Check if a button is selected
-  isButtonSelected(clientName: string): boolean {
-    return this.selectedClientName === clientName;
-  }
+  
 
-  // navigateToFeedbackDetails(clientName: string, visitDate: string): void {
-  //   const selectedFeedback = this.filteredData.find(item => item.client_name === clientName && item.visit_date === visitDate);
-    
+  
+  
+  // handleButtonClick(clientName: string): void {
+  //   const selectedFeedback = this.filteredData.find(item => item.client_name === clientName);
+  //   console.log("selected Feedback=",selectedFeedback);
   //   if (selectedFeedback) {
-  //     this.router.navigate(['/feedback-details'], { state: { feedback: selectedFeedback } });
+  //     this.selectedClientName = clientName;
+  //     this.feedbackDataService.setSelectedFeedback(selectedFeedback);
   //   }
   // }
+
+  // Check if a button is selected
+  // isButtonSelected(clientName: string): boolean {
+  //   return this.selectedClientName === clientName;
+  // }
+
+  
 }
--- src/app/services/feedback-data.service.ts
+++ src/app/services/feedback-data.service.ts
@@ -1,6 +1,6 @@
 // feedback-data.service.ts
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable  } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +8,22 @@ import { BehaviorSubject } from 'rxjs';
 export class FeedbackDataService {
   private selectedFeedbackSource = new BehaviorSubject<any>(null);
   selectedFeedback$ = this.selectedFeedbackSource.asObservable();
-  filteredData: any[] = [];
+
+  private filteredDataSubject: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  filteredData$: Observable<any[]> = this.filteredDataSubject.asObservable();
+  
 
   setSelectedFeedback(feedback: any): void {
     this.selectedFeedbackSource.next(feedback);
   }
   setFilteredData(data: any[]): void {
-    this.filteredData = data;
-    console.log("filtered Visit in service file=",this.filteredData);
+    this.filteredDataSubject.next(data);
+    
   }
 
-  getFilteredData(): any[] {
-    return this.filteredData;
+  getFilteredData(): Observable<any> {
+    //console.log("filtered Visit in service file=",this.filteredData$);
+    return this.filteredData$;
   }
+
 }

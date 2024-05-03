9ab816d3b9a24c40c5efd2b4f85544d3f279a227
livinglabsicets <livinglabsicets@gmail.com>
2024-05-03 15:47:47 +0530

    fix position of button and card on front cover page

--- src/app/component-front-cover-page/component-front-cover-page.component.ts
+++ src/app/component-front-cover-page/component-front-cover-page.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { FeedbackDataService } from '../services/feedback-data.service';
 import { ActivatedRoute } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({

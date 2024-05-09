2af698d4d94ff92b3fe7f8f86b9ac19d735cbf9a
Krishna07039 <gundu.chaitanya@infosys.com>
2024-05-09 10:53:15 +0530

    changes css properties in third and second components

--- src/app/component-page-five/component-page-five.component.ts
+++ src/app/component-page-five/component-page-five.component.ts
@@ -12,6 +12,7 @@ export class ComponentPageFiveComponent implements OnInit {
   overallRating: number = 0;
   comment: string = '';
 
+
   constructor(private feedbackDataService: FeedbackDataService) { }
 
   ngOnInit(): void {
